ded6ad120b96261fa06595ffcc5051514a67d993
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-03 18:45:42 +0530

    Update

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div className="md:grid grid-cols-3 gap-4">
+            <div className=""></div>
+            <div className="py-12 rounded-xl px-8 my-[30%] bg-gray-200">
+                <h1 className="text-center uppercase font-bold text-gray-500 text-xl">Login Here</h1>
+            </div>
+            <div className=""></div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
