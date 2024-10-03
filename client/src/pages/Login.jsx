e3cd9bc3a5c1cdda4dfafcc4ea238de910380309
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-03 18:54:12 +0530

    Update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [LoginData, SetLoginData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setFormData({ ...LoginData, [e.target.name]: e.target.value });
+    };
+
+    const HeadleLogin = (e) => {
+        e.preventDefault()
+
+        console.log(LoginData)
+
+    }
   return (
     <div>
         <div className="md:grid grid-cols-3 gap-4">
             <div className=""></div>
-            <div className="py-12 rounded-xl px-8 my-[30%] bg-gray-200">
+            <div className="md:mx-0 mx-4 py-12 rounded-xl px-8 my-[30%] bg-gray-200">
                 <h1 className="text-center uppercase font-bold text-gray-500 text-xl">Login Here</h1>
+
+                <div className="my-4">
+                    <form method="post" onSubmit={HeadleLogin}>
+                        <div className="py-4">
+                            <input type="email" name="" id="" className="h-12 w-full rounded pl-2" onChange={handleChange} placeholder='Email Address'/>
+                        </div>
+                        <div className="py-4">
+                            <input type="password" name="" id="" className="h-12 w-full rounded pl-2" onChange={handleChange} placeholder='Password'/>
+                        </div>
+
+                        <button type="submit" className='bg-blue-500 text-white py-2 px-4 rounded duration-500 hover:bg-blue-500'>Login Here</button>
+                    </form>
+                </div>
             </div>
             <div className=""></div>
         </div>
